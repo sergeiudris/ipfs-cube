53d856e8e19ad5c3b24e01069515e5e79f88a2d7
Sergei Udris <sergei.udris@gmail.com>
2021-04-16 12:18:09 +0300

    src/ipfs http server

--- src/ipfs/core.go
+++ src/ipfs/core.go
@@ -2,10 +2,19 @@ package ipfsfind_ipfs_core
 
 import (
 	"fmt"
+	"net/http"
 )
 
 //Main _
 func Main() {
-
 	fmt.Println("foo")
+
+	server := http.NewServeMux()
+
+	server.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "foo")
+	})
+
+	http.ListenAndServe("0.0.0.0:8000", server)
+
 }
