f71eb2e60dd4e5472a3663a4e65f4934fadf58f5
Sergei Udris <sergei.udris@gmail.com>
2021-06-30 10:02:04 +0300

    datahike - log/merge-config!, torrent schema

--- app/src/ipfs_shipyard/find/app/db.clj
+++ app/src/ipfs_shipyard/find/app/db.clj
@@ -5,24 +5,45 @@
                                      timeout to-chan  sliding-buffer dropping-buffer
                                      pipeline pipeline-async]]
    [clojure.java.io :as io]
-   [datahike.api]))
+   [cljctools.bytes.runtime.core :as bytes.runtime.core]
+   [cljctools.codec.runtime.core :as codec.runtime.core]
+   [cljctools.bittorrent.spec :as bittorrent.spec]
+   [datahike.api]
+   [taoensso.timbre :as log]
+   [taoensso.timbre.appenders.3rd-party.rotor]))
 
+(log/merge-config! {:level :debug
+                    :min-level :info
+                    :appenders {:rotating (taoensso.timbre.appenders.3rd-party.rotor/rotor-appender
+                                           {:path
+                                            (let [peer-index 1]
+                                              (->
+                                               (io/file (System/getProperty "user.dir") "volumes" (format "peer%s" peer-index) "db-server.log")
+                                               (.getCanonicalFile)))
+                                            :max-size (* 512 1024)
+                                            :backlog 10})}})
 
 (comment
 
   (require
    '[ipfs-shipyard.find.app.db :as find.app.db]
+   '[cljctools.bytes.runtime.core :as bytes.runtime.core]
+   '[cljctools.codec.runtime.core :as codec.runtime.core]
+   '[cljctools.bittorrent.spec :as bittorrent.spec]
    '[datahike.api]
    :reload)
 
-  (let [peer-index 1]
-    (io/make-parents (System/getProperty "user.dir") "volumes" (format "peer%s" peer-index) "db"))
+  (do
+    (let [peer-index 1]
+      (io/make-parents (System/getProperty "user.dir") "volumes" (format "peer%s" peer-index) "db"))
+    (def cfg {:store {:backend :file
+                      :path "./volumes/peer1/db"}})
+    (datahike.api/create-database cfg)
+    (def conn (datahike.api/connect cfg)))
 
-  (def cfg {:store {:backend :file :path "./volumes/peer1/db"}})
-
-  (datahike.api/create-database cfg)
-
-  (def conn (datahike.api/connect cfg))
+  (do
+    (datahike.api/release conn)
+    (datahike.api/delete-database cfg))
 
   (datahike.api/transact conn [{:db/ident :name
                                 :db/valueType :db.type/string
@@ -55,13 +76,47 @@
                     [?e :age ?a]]
                   (datahike.api/history @conn))
 
-  (datahike.api/release conn)
+  ;
+  )
 
-  (datahike.api/delete-database cfg)
+(comment
+
+  (datahike.api/transact conn [{:db/ident ::bittorrent.spec/infohash
+                                :db/valueType :db.type/string
+                                :db/cardinality :db.cardinality/one}
+                               {:db/ident ::bittorrent.spec/metadata
+                                :db/valueType :db.type/ref
+                                :db/cardinality :db.cardinality/one}
+                               {:db/ident ::bittorrent.spec/name
+                                :db/valueType :db.type/string
+                                :db/cardinality :db.cardinality/one}
+                               {:db/ident ::bittorrent.spec/files
+                                :db/valueType :db.type/ref
+                                :db/cardinality :db.cardinality/many}])
+
+  (time
+   (dotimes [n 1000]
+     (when (zero? (mod n 10))
+       (println :n n))
+     (datahike.api/transact conn (map
+                                  (fn [i]
+                                    {::bittorrent.spec/infohash (-> (bytes.runtime.core/random-bytes 20) (codec.runtime.core/hex-to-string))
+                                     ::bittorrent.spec/metadata
+                                     {::bittorrent.spec/name (str "some torrent name details 12312312 foo bar " n "," i)
+                                      ::bittorrent.spec/files (map (fn [j]
+                                                                     {::bittorrent.spec/name (str "some file name details 12312312 foo bar " j)}) (range 0 10))}})
+                                  (range 100)))
+     nil))
+
+  (->
+   (datahike.api/q '[:find ?e ?infohash
+                     :where
+                     [?e ::bittorrent.spec/infohash ?infohash]]
+                   @conn)
+   (count))
+
 
 
   ;
   )
-
-
 
