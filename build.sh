8f59574c3025c7c72a51cf504a719e7253cedc89
Sergei Udris <sergei.udris@gmail.com>
2021-07-12 12:05:04 +0300

    first find build! find_1.0-1_amd64.deb

--- build.sh
+++ build.sh
@@ -32,4 +32,31 @@ lein-uberjar(){
   #  java -Dclojure.core.async.pool-size=1 -jar target/find-standalone.jar
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting build..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input target \
+    --dest target \
+    --main-jar find.standalone.jar \
+    --name "find" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments ipfs-shipyard.find.main \
+    --resource-dir resources \
+    $J_ARG
+  
+}
+
 "$@"
