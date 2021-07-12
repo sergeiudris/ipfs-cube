032ab434620d969b7bd2210cce22e917eb761746
Sergei Udris <sergei.udris@gmail.com>
2021-07-12 10:14:02 +0300

    add build.sh

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+repl(){
+  clj -M:repl
+}
+
+main(){
+  clojure -M:main
+}
+
+compile-java(){
+  lein javac
+}
+
+gen-proto(){
+  OUT=target/proto
+  mkdir -p $OUT
+  SRC=$(cd ../../../ && pwd)/cljctools/ipfs-jvm/src
+  protoc --java_out=${OUT} --proto_path ${SRC}/cljctools/ipfs/runtime node_proto.proto
+}
+
+depstar-uberjar(){
+  clojure -X:depstar uberjar \
+    :aot true \
+    :jar target/find.jar \
+    :verbose true \
+    :main-class ipfs-shipyard.find.main
+}
+
+lein-uberjar(){
+  lein with-profiles +prod uberjar
+  #  java -Dclojure.core.async.pool-size=1 -jar target/find-standalone.jar
+}
+
+"$@"
